593bb61d964d0bae25feaf2443f8ae0781855fd6
ehabhaq-brooks <129895534+ehabhaq-brooks@users.noreply.github.com>
2025-09-05 22:00:46 -0400

    removed forced althode mode

--- components/core/crazyflie/hal/src/sensors_mpu6050_hm5883L_ms5611.c
+++ components/core/crazyflie/hal/src/sensors_mpu6050_hm5883L_ms5611.c
@@ -536,7 +536,7 @@ static void sensorsDeviceInit(void)
     }
 #endif
 
-    setCommandermode(ALTHOLD_MODE);
+    //setCommandermode(ALTHOLD_MODE);
     DEBUG_PRINTI("Initialized in ALTHOLD_MODE");
     DEBUG_PRINTI("sensors init done");
     /*
